5ff6d94c908518f707728e9a9b8c884294049b25
Heru subekti <heroe_soebekti@yahoo.co.id>
2026-01-24 23:19:59 +0700

    initial commit

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+define('BASE_URL', 'http://localhost/zlib');
+define('BASE_PATH', dirname(__DIR__) . '/public');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'zlibrary');
+define('DB_DRIVER', 'pdo');
+
+define('APP_LANG', 'en_US');
+define('LANGUAGES_PATH', ROOT_PATH . '/languages');
+
+define('ROOT_PATH', realpath(dirname(__DIR__) . '/'));
+define('FCPATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
