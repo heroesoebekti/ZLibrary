4cd6c6e13780a0af383a793afe5e56487753a25f
Heru subekti <heroe_soebekti@yahoo.co.id>
2026-01-25 14:50:10 +0700

    security: harden LanguageController against open redirect and improve cookie security

--- app/Controllers/Public/LanguageController.php
+++ app/Controllers/Public/LanguageController.php
@@ -6,22 +6,31 @@ use App\Core\Controller;
 class LanguageController extends Controller {
     
     public function switch($lang) {
-        $allowed = ['id_ID', 'en_US'];
-        
+        $allowed = ['id_ID', 'en_US'];       
         if (in_array($lang, $allowed)) {
             $referer = $_SERVER['HTTP_REFERER'] ?? '';
-
             if (strpos($referer, '/admin') !== false) {
                 $_SESSION['app_lang'] = $lang;
             } else {
-                setcookie('app_lang', $lang, time() + (86400 * 30), "/", "", false, true);
-                $_COOKIE['app_lang'] = $lang;
+                setcookie('app_lang', $lang, [
+                    'expires' => time() + (86400 * 30),
+                    'path' => '/',
+                    'domain' => '',
+                    'secure' => isset($_SERVER['HTTPS']),
+                    'httponly' => true,
+                    'samesite' => 'Lax'
+                ]);
             }
         }
-
-        $redirect = (isset($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'], $_SERVER['HTTP_HOST']) !== false) 
-                    ? $_SERVER['HTTP_REFERER'] 
-                    : BASE_URL;
+        $redirect = BASE_URL;
+        
+        if (isset($_SERVER['HTTP_REFERER'])) {
+            $refererHost = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
+            $serverHost = $_SERVER['HTTP_HOST'];
+            if ($refererHost === $serverHost) {
+                $redirect = str_replace(["\r", "\n"], '', $_SERVER['HTTP_REFERER']);
+            }
+        }
 
         header("Location: " . $redirect);
         exit;
